1a439fdf317539d77a313301a73aba7de645119b
YuryEreshchenko <t114012010@ntut.edu.tw>
2025-11-11 12:20:06 +0800

    Add blinking modes

--- HMW3_2_Section5/HMW3_2_Section5.ino
+++ HMW3_2_Section5/HMW3_2_Section5.ino
@@ -1,212 +1,105 @@
-const int RedLEDPin = 9;
+const int RedLEDPin   = 9;
 const int GreenLEDPin = 10;
-const int BlueLEDPin = 11;
+const int BlueLEDPin  = 11;
 
-const int pushButton = 2;
+const int pushButton  = 2;
 
-int buttonState = 0;
+int  buttonState    = 0;
+bool buttonPressed  = false;
 
-unsigned long pressingTime = 0;
-const long longPressInterval = 1000;
-bool buttonPressed = false; 
+unsigned long pressingTime           = 0;
+const long     longPressInterval     = 1000;  // ms
 
-int ledCounter = 1;           // Tracks current color (1-7)
-
-//Mode 0 : Constant Light
-//Mode 1 : Blick mode 
-//Mode 2 : Breathing light
-int currentMode = 0;
+int currentMode = 1;  // 1 = ON, 2/3/4 = blinking speeds
 
 unsigned long blinkTimer = 0;
-const int blinkInterval = 500;
-bool blinkOn = true;
-
-int RCurrentColor = 0;
-int GCurrentColor = 0;
-int BCurrentColor = 0;
-
-
-
-// Fading mode variables
-unsigned long fadeTimer = 0;
-const int fadeInterval = 15;  // Update every 15ms for smooth fading
-int fadeBrightness = 0;
-int fadeDirection = 1;        // 1 for increasing, -1 for decreasing
-const int fadeStep = 5;       // How much to change brightness each step
-
-
+bool          blinkOn    = true;
 
 void setup() {
-
   Serial.begin(9600);
-  pinMode(RedLEDPin, OUTPUT);
+
+  pinMode(RedLEDPin,   OUTPUT);
   pinMode(GreenLEDPin, OUTPUT);
-  pinMode(BlueLEDPin, OUTPUT);
-  pinMode(pushButton, INPUT);
-
+  pinMode(BlueLEDPin,  OUTPUT);
+  pinMode(pushButton,  INPUT);
 }
-
-
 
 void loop() {
-
   int clickType = checkButton();
 
-  // if ( clickType == 1){
-  //   changeLEDColor();
-  // }
-
-  else if (clickType == 1){
+  // Change mode on any valid click
+  if (clickType == 1) {
     currentMode++;
-    if (currentMode >= 3){
-      currentMode = 0;
+    if (currentMode > 4) {
+      currentMode = 1;
     }
-    if(currentMode == 1){
-      blinkTimer = 0;
-      blinkOn = true;
-    }
-    
-
   }
-  
-  updateLEDColor(currentMode);
 
-}
-
-
-
-void updateLEDColor(int currentMode){
-
-    //Static mode
-    if (currentMode == 0){
-    Serial.println("Mode 0 : Static light");
-    //Nothing static light
-    }
-    ///////////////////////////////////////////////////////
-    
-    //Blink mode
-    else if (currentMode == 1){
-    Serial.println("Mode 1 : Blink");
-    unsigned long currentTime = millis();
-    if(currentTime - blinkTimer > blinkInterval){
-      blinkOn = !blinkOn;
-      blinkTimer = currentTime;
-    }
-    if (blinkOn == true){
-      PowerLEDControl(RCurrentColor, GCurrentColor, BCurrentColor);
-    }
-    else{
-      PowerLEDControl(255, 255, 255);
-    }
-    }
-    ///////////////////////////////////////////////////////
-
-    //Fading mode
-    else if(currentMode == 2){
-
-      unsigned long currentTime = millis();
-      
-      if(currentTime - fadeTimer > fadeInterval){
-        fadeBrightness += fadeDirection * fadeStep;
-        
-        // Reverse direction at the limits
-        if(fadeBrightness >= 255){
-          fadeBrightness = 255;
-          fadeDirection = -1;
-        }
-        else if(fadeBrightness <= 0){
-          fadeBrightness = 0;
-          fadeDirection = 1;
-        }
-        
-        fadeTimer = currentTime;
-        
-        // Apply fading to current color
-        int fadeR = map(fadeBrightness, 0, 255, 255, RCurrentColor);
-        int fadeG = map(fadeBrightness, 0, 255, 255, GCurrentColor);
-        int fadeB = map(fadeBrightness, 0, 255, 255, BCurrentColor);
-        
-        PowerLEDControl(fadeR, fadeG, fadeB);
-      }
-    }
-
-}
-
-
-void changeLEDColor(){
-    
-    ledCounter++;
-
-    if (ledCounter == 5){
-    ledCounter = 0;
-    } 
-    if (ledCounter == 0){
+  // Run current mode
+  if (currentMode == 1) {
+    // Constant ON (inverted LED: 0 = ON)
     PowerLEDControl(0, 0, 0);
-    RCurrentColor = 0;
-    GCurrentColor = 0;
-    BCurrentColor = 0;
-    }
-    if (ledCounter == 1){
-    PowerLEDControl(0, 255, 255);
-    RCurrentColor = 0;
-    GCurrentColor = 255;
-    BCurrentColor = 255;
-    }
-    if (ledCounter == 2){
-    PowerLEDControl(255, 0, 255);
-    RCurrentColor = 255;
-    GCurrentColor = 0;
-    BCurrentColor = 255;
-    }
-    if (ledCounter == 3){
-    PowerLEDControl(255, 255, 0);
-    RCurrentColor = 255;
-    GCurrentColor = 255;
-    BCurrentColor = 0;
-    }
-    if (ledCounter == 4){
-    PowerLEDControl(255, 255, 255);
-    RCurrentColor = 255;
-    GCurrentColor = 255;
-    BCurrentColor = 255;
-    }     
-
+  }
+  else if (currentMode == 2) {
+    Blinking(1000);  // slow
+  }
+  else if (currentMode == 3) {
+    Blinking(500);   // medium
+  }
+  else if (currentMode == 4) {
+    Blinking(50);    // fast
+  }
 }
 
-
-int checkButton(){
+// Returns 1 on short *or* long press release, 0 otherwise
+int checkButton() {
   int clickType = 0;
-  buttonState = !digitalRead(pushButton);
-  delay(5);
 
-  if (buttonState == HIGH && !buttonPressed){
-    pressingTime = millis();
+  // Inverted logic: button wired so !digitalRead() is pressed
+  buttonState = !digitalRead(pushButton);
+  delay(5);  // simple debounce
+
+  if (buttonState == HIGH && !buttonPressed) {
+    pressingTime  = millis();
     buttonPressed = true;
   }
-  if (buttonState == LOW && buttonPressed){
+
+  if (buttonState == LOW && buttonPressed) {
     unsigned long currentTime = millis();
-    if (currentTime - pressingTime < longPressInterval){
+
+    if (currentTime - pressingTime < longPressInterval) {
       Serial.println("Short click");
       clickType = 1;
-    }
-    else {
+    } else {
       Serial.println("Long Press");
-      clickType = 2;
+      clickType = 1;
     }
+
     buttonPressed = false;
   }
 
   return clickType;
-
 }
-
-
 
 void PowerLEDControl(int red, int green, int blue) {
-  analogWrite(RedLEDPin, red);
+  analogWrite(RedLEDPin,   red);
   analogWrite(GreenLEDPin, green);
-  analogWrite(BlueLEDPin, blue);
+  analogWrite(BlueLEDPin,  blue);
 }
 
+void Blinking(int interval) {
+  unsigned long currentTime = millis();
 
+  if (currentTime - blinkTimer > (unsigned long)interval) {
+    if (blinkOn) {
+      // Inverted LED: 0 = ON
+      PowerLEDControl(0, 0, 0);
+    } else {
+      // Inverted LED: 255 = OFF
+      PowerLEDControl(255, 255, 255);
+    }
 
+    blinkOn    = !blinkOn;
+    blinkTimer = currentTime;
+  }
+}
